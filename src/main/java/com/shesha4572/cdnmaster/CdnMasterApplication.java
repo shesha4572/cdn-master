95fe818aa24e6abb01e5c93ea89b9218eefbc397
shesha4572 <shesha4572@gmail.com>
2023-10-24 15:19:10 +0530

    Initial commit

---
+++ src/main/java/com/shesha4572/cdnmaster/CdnMasterApplication.java
@@ -0,0 +1,13 @@
+package com.shesha4572.cdnmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdnMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shesha4572/cdnmaster/CdnMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shesha4572.cdnmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdnMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
